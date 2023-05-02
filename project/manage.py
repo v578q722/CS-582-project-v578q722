72feff10024d013ac875cbf2bb60f71e52ded2ca
v578q722 <130708573+v578q722@users.noreply.github.com>
2023-05-02 16:49:13 -0500

    Update manage.py

--- project/manage.py
+++ project/manage.py
@@ -473,3 +473,59 @@ class Migration(migrations.Migration):
             field=models.BooleanField(default=False),
         ),
     ]
+
+from django.contrib import admin
+
+from .models import Product
+
+admin.site.register(Product)
+
+from django.apps import AppConfig
+
+class ProductsConfig(AppConfig):
+    name = 'products'
+
+from django import forms
+
+from .models import Product
+
+class ProductForm(forms.ModelForm):
+    title = forms.CharField(label='', 
+                    widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    description = forms.CharField(
+                        required=False, 
+                        widget=forms.Textarea(
+                                attrs={
+                                    "placeholder": "Your description",
+                                    "class": "new-class-name two",
+                                    "id": "my-id-for-textarea",
+                                    "rows": 20,
+                                    'cols': 120
+                                }
+                            )
+                        )
+    price = forms.DecimalField(initial=199.99)
+    
+    class Meta:
+        model = Product
+        fields = [
+            'title',
+            'description',
+            'price'
+        ]
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    description = forms.CharField(
+                        required=False, 
+                        widget=forms.Textarea(
+                                attrs={
+                                    "placeholder": "Your description",
+                                    "class": "new-class-name two",
+                                    "id": "my-id-for-textarea",
+                                    "rows": 20,
+                                    'cols': 120
+                                }
+                            )
+                        )
+    price = forms.DecimalField(initial=199.99)
