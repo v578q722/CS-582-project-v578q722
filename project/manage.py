f1ce57fe4f0fb387b7a22da5b5dbc5e269f2bb1a
v578q722 <130708573+v578q722@users.noreply.github.com>
2023-05-02 16:39:12 -0500

    Update manage.py

--- project/manage.py
+++ project/manage.py
@@ -202,3 +202,33 @@ class CourseModelForm(forms.ModelForm):
         if title.lower() == 'abc':
             raise forms.ValidationError("This is not a valid title")
         return title
+    
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=120)
+
+from django.test import TestCase
+
+from django.urls import path
+from .views import (
+    CourseView,
+    CourseListView,
+    CourseCreateView,
+    CourseUpdateView,
+    CourseDeleteView
+    # my_fbv
+)
+
+app_name = 'courses'
+urlpatterns = [
+    path('', CourseListView.as_view(), name='courses-list'),
+    # path('', my_fbv, name='courses-list'),
+    
+
+    path('create/', CourseCreateView.as_view(), name='courses-create'),
+    path('<int:id>/', CourseView.as_view(), name='courses-detail'),
+    path('<int:id>/update/', CourseUpdateView.as_view(), name='courses-update'),
+    path('<int:id>/delete/', CourseDeleteView.as_view(), name='courses-delete'),
+]
